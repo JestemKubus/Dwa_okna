f952f47f2d2e9c1cc4070beb65021a6b0f14d002
Kołodziejczyk Jakub <kolodziejczyk.jakub@SZKOLA.ZST-RADOM.EDU.PL>
2023-10-09 16:22:55 +0200

    nie przesyłanie loginu do drugiego okna

---
+++ app/src/main/java/com/example/newactivity/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.newactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+        Button ButtonLogIn;
+        TextInputEditText login;
+        TextInputEditText password;
+        String LoginTym = "test";
+        String PassTym = "test";
+        TextView information;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = findViewById(R.id.textInputEdit);
+        password = findViewById(R.id.textInputEditPassword);
+        ButtonLogIn = findViewById(R.id.zaloguj);
+        information = findViewById(R.id.information);
+        ButtonLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(login.getText().toString().equals(LoginTym) && password.getText().toString().equals(PassTym))
+                {
+                    Intent intent = new Intent(MainActivity.this, secondView.class);
+                    startActivity(intent);
+                    intent.putExtra("login",login.getText());
+                }
+                else
+                {
+                    information.setText("Podano złe dane");
+                }
+
+
+            }
+        });
+    }
+
+
+//pobieranie wartości z inputa login: login.getText(), hasło:password.getText()
+}
---
+++ app/src/main/java/com/example/newactivity/secondView.java
@@ -0,0 +1,24 @@
+package com.example.newactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class secondView extends AppCompatActivity {
+TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_view);
+
+        textView = findViewById(R.id.textView);
+        String LoginValue = getIntent().getStringExtra("login");
+
+        if(LoginValue != null)
+        {
+            textView.setText(LoginValue);
+        }
+
+    }
+}
